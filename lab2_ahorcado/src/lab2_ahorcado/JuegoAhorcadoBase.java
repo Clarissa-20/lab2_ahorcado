6c9b4b793ff6583fd9fe37b4041f3eff4476b87d
DJCC <david.carcamo2006@gmail.com>
2025-11-07 08:30:23 -0600

    Creacion de interface y clase abstracta

---
+++ lab2_ahorcado/src/lab2_ahorcado/JuegoAhorcadoBase.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2_ahorcado;
+
+/**
+ *
+ * @author David
+ */
+public abstract class JuegoAhorcadoBase implements JuegoAhorcado{
+    
+}
